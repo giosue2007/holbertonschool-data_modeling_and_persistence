0782731077440689761a62d7da33d0c50b1a4959
giosue2007 <toxicgiosue@gmail>
2026-06-04 13:03:22 +0200

    add perm and c

--- sql_join/10-students-and-registered-courses.sql
+++ sql_join/10-students-and-registered-courses.sql
@@ -1,5 +1,5 @@
-SELECT students.name, courses.name
+SELECT students.name, course.title
 FROM students
 JOIN registrations ON students.id = registrations.student_id
-JOIN courses ON registrations.course_id = courses.id
-ORDER BY students.name ASC, courses.name ASC;
+JOIN course ON registrations.course_id = course.id
+ORDER BY students.name ASC, course.title ASC;
