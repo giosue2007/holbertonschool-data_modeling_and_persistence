721502ec9e69912d9c423e7e687b98b316165e32
giosue2007 <toxicgiosue@gmail>
2026-06-04 12:53:17 +0200

    add perm and c

--- sql_join/10-students-and-registered-courses.sql
+++ sql_join/10-students-and-registered-courses.sql
@@ -1,5 +1,5 @@
-SELECT courses.title AS course_title, COUNT(registrations.student_id) AS registration_count
-FROM courses
-LEFT JOIN registrations ON courses.id = registrations.course_id
-GROUP BY courses.id, courses.title
-ORDER BY registration_count DESC, course_title ASC;
+SELECT students.name AS student_name, courses.title AS course_title
+FROM students
+JOIN registrations ON students.id = registrations.student_id
+JOIN courses ON registrations.course_id = courses.id
+ORDER BY student_name ASC, course_title ASC;
